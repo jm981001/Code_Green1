0963ed26322982302293c150fc995cdc87fc4e6b
sonjungmin <sjm981001@gmail.com>
2022-11-04 15:21:21 +0900

    controller, mapper, service 추가 -지영요청

---
+++ Code_Green/src/main/java/com/itwillbs/Code_Green/vo/File_ItemVO.java
@@ -0,0 +1,46 @@
+package com.itwillbs.Code_Green.vo;
+
+public class File_ItemVO {
+ 
+private String  file_item_idx;
+ private String  file;
+ private String  real_file;
+ private String  rf_item_idx;
+ 
+ public String getFile_item_idx() {
+		return file_item_idx;
+	}
+	public void setFile_item_idx(String file_item_idx) {
+		this.file_item_idx = file_item_idx;
+	}
+	public String getFile() {
+		return file;
+	}
+	public void setFile(String file) {
+		this.file = file;
+	}
+	public String getReal_file() {
+		return real_file;
+	}
+	public void setReal_file(String real_file) {
+		this.real_file = real_file;
+	}
+	public String getRf_item_idx() {
+		return rf_item_idx;
+	}
+	public void setRf_item_idx(String rf_item_idx) {
+		this.rf_item_idx = rf_item_idx;
+	}
+	
+	public File_ItemVO() {
+		// TODO Auto-generated constructor stub
+	}
+	public File_ItemVO(String file_item_idx, String file, String real_file, String rf_item_idx) {
+		super();
+		this.file_item_idx = file_item_idx;
+		this.file = file;
+		this.real_file = real_file;
+		this.rf_item_idx = rf_item_idx;
+	}
+	
+}
